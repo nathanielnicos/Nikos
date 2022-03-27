b2bc8d7539c14e8ba6ea904b312b1cae89f361a4
Nathaniel Nico Sugiono <nathanielnicos@gmail.com>
2022-03-27 16:48:56 +0700

    Fix request location permission

--- app/src/main/java/id/nns/nikos/map/MapFragment.kt
+++ app/src/main/java/id/nns/nikos/map/MapFragment.kt
@@ -1,14 +1,15 @@
 package id.nns.nikos.map
 
 import android.Manifest
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.vmadalin.easypermissions.EasyPermissions
-import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import id.nns.nikos.R
 import id.nns.nikos.databinding.FragmentMapBinding
 
 class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
@@ -33,16 +34,43 @@ class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
         setViewVisibility()
 
-        binding.btnRequestPermission.setOnClickListener {
+        binding.btnClick.setOnClickListener {
             requestLocationPermission()
         }
     }
 
-    private fun hasLocationPermission() = EasyPermissions.hasPermissions(
-        requireContext(),
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
+    private fun setViewVisibility() {
+        when {
+            EasyPermissions.hasPermissions(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                binding.tvStatusValue.text = getString(R.string.fine_location_permission_granted)
+                binding.tvMessageValue.text = getString(R.string.no_value)
+
+                binding.btnClick.isEnabled = false
+                binding.btnClick.isClickable = false
+                binding.btnClick.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.light_grey
+                    )
+                )
+            }
+            EasyPermissions.hasPermissions(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) -> {
+                binding.tvStatusValue.text = getString(R.string.coarse_location_permission_granted)
+                binding.tvMessageValue.text =
+                    getString(R.string.request_fine_location_permission_now)
+            }
+            else -> {
+                binding.tvStatusValue.text = getString(R.string.no_value)
+                binding.tvMessageValue.text = getString(R.string.click_the_button)
+            }
+        }
+    }
 
     private fun requestLocationPermission() {
         EasyPermissions.requestPermissions(
@@ -71,31 +99,11 @@ class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            SettingsDialog.Builder(requireContext()).build().show()
-        } else {
-            requestLocationPermission()
-        }
+        requestLocationPermission()
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Toast.makeText(
-            requireContext(),
-            "Permission granted!",
-            Toast.LENGTH_SHORT
-        ).show()
-
         setViewVisibility()
-    }
-
-    private fun setViewVisibility() {
-        if (hasLocationPermission()) {
-            binding.tvPermissionGranted.visibility = View.VISIBLE
-            binding.btnRequestPermission.visibility = View.GONE
-        } else {
-            binding.tvPermissionGranted.visibility = View.GONE
-            binding.btnRequestPermission.visibility = View.VISIBLE
-        }
     }
 
     override fun onDestroyView() {
